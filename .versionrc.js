82c0e5de92af8785eca864296b556cec41c50f3d
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-10-27 15:50:01 +0200

    chore: add changelog config file

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features', hidden: false },
+    { type: 'fix', section: 'Bug Fixes', hidden: false },
+    { type: 'chore', section: 'Chores', hidden: false },
+    { type: 'build', section: 'Build System - External Deps', hidden: false },
+    { type: 'docs', section: 'Documentation', hidden: false },
+    { type: 'refactor', section: 'Refactoring', hidden: false },
+    { type: 'style', section: 'Formatting & Style', hidden: false },
+    { type: 'perf', section: 'Improvements', hidden: false },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'ci', section: 'CI/CD', hidden: false },
+  ],
+}
